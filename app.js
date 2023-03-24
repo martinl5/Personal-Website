38afa701bfd0f2c00da140432b7f4636ee02ed81
martinl5 <102524041+martinl5@users.noreply.github.com>
2023-03-24 17:57:51 +0800

    Updated Submit button and Projects

--- app.js
+++ app.js
@@ -7,8 +7,8 @@
             document.getElementById(button.dataset.id).classList.add("active");
         })
     });
+    
     document.querySelector(".theme-btn").addEventListener("click", () => {
         document.body.classList.toggle("light-mode");
     })
 })();
-
